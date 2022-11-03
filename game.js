b7b78510ca779e0a3b1eb84a20eb1308df6c5db0
Chillsbro <chillsbro@users.noreply.github.com>
2022-11-03 10:46:31 -0500

    add comp choice and var table

---
+++ game.js
@@ -0,0 +1,18 @@
+let values = {
+  0: "rock",
+  1: "paper",
+  2: "scissors",
+  rock: 0,
+  paper: 1,
+  scissors: 2,
+};
+
+function getComputerChoice() {
+  return values[Math.floor(Math.random() * 2)];
+}
+
+console.log(getComputerChoice());
+
+function playerChoice() {
+  addEventListener;
+}
