f6bc1f1d4d44d9599bbea71ab38f4f50adfd1bb1
Chillsbro <114181571+Chillsbro@users.noreply.github.com>
2022-11-04 14:56:12 -0500

    added reset function to clear console and text when game is reset, added increment for tracking player/cpu wins

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-let values = {
+const values = {
   0: "rock",
   1: "paper",
   2: "scissors",
@@ -7,7 +7,7 @@ let values = {
   scissors: 2,
 };
 
-let victory = {
+const victory = {
   [values.rock]: values.scissors,
   [values.scissors]: values.paper,
   [values.paper]: values.rock,
@@ -17,44 +17,70 @@ const gameConfig = {
   totalRounds: 5,
 };
 
-let gameState = {
+const gameState = {
   roundsPlayed: [], // should look like ['cpu', 'player', 'cpu', 'draw', 'draw']
+  //my wins/losses
+  playerWins: 0,
+  cpuWins: 0,
 };
 
 const gamesListElement = document.getElementById("games");
 gamesListElement.innerText = JSON.stringify(gameState.roundsPlayed);
 
 function getComputerMove() {
-  return Math.floor(Math.random() * 2);
+  return Math.floor(Math.random() * 3);
 }
 
 // add parameter for player's move
 function doPlayerMove(playerChoice) {
   console.log(`player made move: ${values[playerChoice]}`);
-
   // generate computer move
   let cpuChoice = getComputerMove();
 
   // check if player move trumps computer move
   // add 'cpu' or 'player' or 'draw' (whoever won) to gameState.roundsPlayed
   if (victory[playerChoice] === cpuChoice) {
-    gameState.roundsPlayed.push("player"); // I WIN!!!! [this round, at least.... muheheeheh]
+    gameState.roundsPlayed.push("You Win"); // I WIN!!!! [this round, at least.... muheheeheh]
+    gameState.playerWins++;
   } else if (victory[cpuChoice] === playerChoice) {
-    gameState.roundsPlayed.push("cpu"); // CPU WINS. bleh. stupid.
+    gameState.roundsPlayed.push("CPU won"); // CPU WINS. bleh. stupid.
+    gameState.cpuWins++;
   } else {
-    gameState.roundsPlayed.push("draw"); // must be a draw!
+    gameState.roundsPlayed.push("Tied"); // must be a draw!
   }
 
   gamesListElement.innerText = JSON.stringify(gameState.roundsPlayed);
 
+  if (gameState.roundsPlayed.length === gameConfig.totalRounds) {
+    doEndGame();
+    return;
+  }
   // check if we're at the end of the game
 
   // end the game and tell the player who won
   // set up for next game, maybe
 }
-
+function doEndGame() {
+  rockbtn.disabled = true;
+  paperbtn.disabled = true;
+  scissorsbtn.disabled = true;
+  if (gameState.playerWins > gameState.cpuWins) {
+    winLose.innerText = "you won";
+  } else if (gameState.cpuWins > gameState.playerWins) {
+    winLose.innerText = "you lose";
+  } else {
+    winLose.innerText = "draw";
+  }
+  console.log(gameState);
+}
 function doResetRound() {
   gameState.roundsPlayed = [];
+  gameState.playerWins = 0;
+  gameState.cpuWins = 0;
+  rockbtn.disabled = false;
+  paperbtn.disabled = false;
+  scissorsbtn.disabled = false;
+  winLose.innerText = " ";
 }
 
 // in each of these, call playerChoice(*choice*) via an arrow function
@@ -76,3 +102,10 @@ document.getElementById("reset").addEventListener("click", () => {
 addEventListener("blah", () => {
   // execute this code when `blah` is fired
 });
+
+const rockbtn = document.getElementById("rock");
+const paperbtn = document.getElementById("paper");
+const scissorsbtn = document.getElementById("scissors");
+//const winLose = document.getElementById("winlose");
+const winLose = document.querySelector("#winlose");
+console.log(winLose);
